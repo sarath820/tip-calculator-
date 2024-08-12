07f7300a11cedcaabd9fc51e304e074f17cd8e49
sarath kumar <sarathkumar232000@gmail.com>
2024-08-12 20:41:03 +0530

    project completed

--- myjava.js
+++ myjava.js
@@ -1,6 +1,5 @@
 let boxvalue = 0;
 let personbox = 0;
-// let custombox = 0
 document.querySelector(".box1").addEventListener("input",function() {
     boxvalue = this.value;
 })
@@ -14,10 +13,15 @@ document.querySelector(".box").addEventListener("input",function(){
 })
 document.querySelectorAll(".tip *").forEach((element)=>{
    element.addEventListener("click", function (){
+          let numberstring = 0;
+         if (this.className === "custom"){
+            numberstring = this.value;
+         }else{
+            ht = this.innerHTML;
+            let partstring = ht.substring(0,2);
+            numberstring = +partstring;
+         }
         
-         ht = this.innerHTML;
-         let partstring = ht.substring(0,2);
-         let numberstring = +partstring;
 
         let total = (boxvalue * numberstring)/100;
         let perperson = total/personbox;
